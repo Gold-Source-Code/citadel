0161dbcea243eb6de9f7f01a9ababc399b266b54
CB <89779186+Gold-Source-Code@users.noreply.github.com>
2025-09-15 09:15:38 +0200

    Added first page (non-functional)

---
+++ Fabriek/resources/views/mainpage.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Fabriek</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/mainstyling.css')}}">
+    <head\>
+    <body>
+        <div class="watermark">
+            <img src="{{asset('icons/cooler.gif')}}" alt="watermark">
+        </div>
+    <body\>
+<html\>
