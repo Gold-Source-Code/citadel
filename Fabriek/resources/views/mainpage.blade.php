d2f05f4f274f6ec77334cd99b03a7df3eb485434
CB <89779186+Gold-Source-Code@users.noreply.github.com>
2025-09-18 15:49:42 +0200

    Plattegrond en klik velden toegevoegt

--- Fabriek/resources/views/mainpage.blade.php
+++ Fabriek/resources/views/mainpage.blade.php
@@ -4,8 +4,18 @@
         <link rel="stylesheet" type="text/css" href="{{asset('css/mainstyling.css')}}">
     <head\>
     <body>
+        <map name="mapfields">
+             <area shape="rect" coords="761,407,852,485" alt="schaar" href="/schaar">
+             <area shape="rect" coords="761,330,794,399" alt="platenrek" href="/platenrek">
+             <area shape="rect" coords="761,242,814,322" alt="kantbank" href="/kantbank">
+        </map>
+
+        <div class="map">
+            <img src="{{asset('icons/plattegrondv1.png')}}" alt="map" usemap="#mapfields">
+        </div>
+
         <div class="watermark">
-            <img src="{{asset('icons/cooler.gif')}}" alt="watermark">
+            <img src="{{asset('icons/watermark5.png')}}" alt="watermark">
         </div>
     <body\>
 <html\>
