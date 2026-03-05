4c5d20fb99bafd5206123843c01e02c98126427d
CB <89779186+Gold-Source-Code@users.noreply.github.com>
2026-03-05 11:36:23 +0100

    Test Version 2.0
    This version will be used in local enviroment.
    
    - Added connection to new table.

--- Fabriek/app/Http/Controllers/MainController.php
+++ Fabriek/app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Main;
+use App\Models\Machine;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -10,14 +11,26 @@ class MainController extends Controller
     public function mainpage()
     {
         $allInfo = Main::all();
+        $extraInfo = Machine::all();
         $foto = Main::where('id', 1)->value('foto');
         $datum = Main::where('id', 1)->value('datum');
         $video = Main::where('id', 1)->value('video');
         $extra = Main::where('id', 1)->value('extra');
         $contact = Main::where('id', 1)->value('contact');
+        $photo = Machine::where('id', 1)->value('photo');
         return view('mainpage', ['extrainfo' => $extra, 'video' => $video, 'datum' => $datum, 'foto' => $foto, 'contact' => $contact]);
     }
 
+    public function stringtolist($string)
+    {
+        $input = $string;
+
+        $output = json_decode($input, true);
+        $output = array_merge(...$output);
+        return ($output);
+    }
+
+    /* 
     public function schaar()
     {
         $allInfo = Main::all();
@@ -27,5 +40,6 @@ class MainController extends Controller
         $extra = Main::where('id', 1)->value('extra');
         $contact = Main::where('id', 1)->value('contact');
         return view('schaar', ['extrainfo' => $extra, 'video' => $video, 'datum' => $datum, 'foto' => $foto, 'contact' => $contact]);
-    }
+    } 
+    */
 }
