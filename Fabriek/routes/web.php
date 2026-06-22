b87777b60baf60b4c3b5016d1f4a0507f1ac4750
CB <89779186+Gold-Source-Code@users.noreply.github.com>
2026-06-22 13:21:32 +0200

    v2.1 - Gemiste Comments Toegevoegt
    Zie tiitel

--- Fabriek/routes/web.php
+++ Fabriek/routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 // Alle functies staan in Http/Controllers/MainController.php
-Route::get('/mainpage', [MainController::class, "mainpage"])->name("main");
-Route::get('/schaar', [MainController::class, "schaar"]);
-Route::get('/kantbank', [MainController::class, "kantbank"]);
+Route::get('/mainpage', [MainController::class, "mainpage"])->name("main"); // http://localhost:8000/mainpage
+Route::get('/schaar', [MainController::class, "schaar"]); // http://localhost:8000/schaar
+Route::get('/kantbank', [MainController::class, "kantbank"]); // http://localhost:8000/schaar
